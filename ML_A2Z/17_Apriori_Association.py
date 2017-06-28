3af311a9232e6a6dea36ea4867546219c8f212fa
Atul Singh <victimizeit@gmail.com>
2017-06-28 11:31:22 +0530

    c

--- ML_A2Z/17_Apriori_Association.py
+++ ML_A2Z/17_Apriori_Association.py
@@ -8,11 +8,21 @@ Apriori Association
 """
 
 import pandas as pd
-#from sklearn.
+from apyori import apriori
 
 # reading the data file
 data = pd.read_csv('dataset/Market_Basket_Optimisation.csv', header=None)
 
 transaction=[]
-for i in range(len(data)):
-    transaction.append(list(data.iloc[i].values))
+for i in range(0, 7501):#range(len(data)):
+    #transaction.append(list(data.iloc[i].values))
+    transaction.append([str(data.values[i,j]) for j in range(0,20)])
+    
+# implementing
+clf = apriori(transaction, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+result = list(clf)
+
+    
+
+ 
